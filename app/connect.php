7b70f05c5db750c4ee85c58688844ee343e49d41
Vsevolod Kurochka <seva.kurochka@gmail.com>
2020-08-05 14:24:17 +0300

    partners

--- app/connect.php
+++ app/connect.php
@@ -1,5 +1,8 @@
 <?php
 	require_once 'vendor/autoload.php';
 	$loader = new Twig_Loader_Filesystem('views');
-	$twig = new Twig_Environment($loader);
+	$twig = new Twig_Environment($loader, [
+	    'debug' => true
+    ]);
+    $twig->addExtension(new Twig_Extension_Debug());
 	$twig->addGlobal('path', 'assets/build');
